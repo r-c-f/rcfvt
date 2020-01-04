35c19036a3a309d7653eaa61743cb508d963bce7
Ryan Farley <ryan.farley@gmx.com>
2020-01-04 11:04:21 -0600

    use separate fdio_full header actually
    Handles more edge cases that we'll never actually encounter and I like
    it more anyway.

---
+++ src/fdio_full.h
@@ -0,0 +1,49 @@
+
+#ifndef FDIO_FULL_H_INC
+#define FDIO_FULL_H_INC
+
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+/* functions to do read/write without shortness */
+static bool read_full(int fd, void *buf, size_t count)
+{
+	ssize_t ret;
+	char *pos;
+
+	for (pos = buf; count;) {
+		errno = 0;
+		ret = read(fd, pos, count > SSIZE_MAX ? SSIZE_MAX : count);
+		if (ret < 1) {
+			if (errno == EINTR)
+				continue;
+			return false;
+		}
+		pos += ret;
+		count -= ret;
+	}
+	return true;
+}
+static bool write_full(int fd, const void *buf, size_t count)
+{
+	ssize_t ret;
+	const char *pos;
+
+	for (pos = buf; count;) {
+		errno = 0;
+		ret = write(fd, pos, count > SSIZE_MAX ? SSIZE_MAX : count);
+		if (ret < 1) {
+			if (errno == EINTR)
+				continue;
+			return false;
+		}
+		pos += ret;
+		count -= ret;
+	}
+	return true;
+}
+
+#endif
